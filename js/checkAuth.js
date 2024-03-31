ca27dbcdfc9a81bbeb0a284d8cb6251e79dbae7d
Bralin-Bo <110297512+Bralin-Bo@users.noreply.github.com>
2024-04-01 00:43:40 +0500

    Fix url

--- js/checkAuth.js
+++ js/checkAuth.js
@@ -2,11 +2,13 @@
 function changeCurrentPage() {
     let auth = true
     const currentPage = window.location.href
+    const pageURL = 'https://bralin-bo.github.io/getbonus/index.html'
+    // const pageURL = 'http://127.0.0.1:5500/index.html'
     console.log(currentPage);
-    if (currentPage == 'http://127.0.0.1:5500/index.html' && auth==false) {
+    if (currentPage == pageURL && auth==false) {
         window.location.href = '/registration.html'
     }
-    else if(currentPage != 'http://127.0.0.1:5500/index.html' && auth==true){
+    else if(currentPage != pageURL && auth==true){
         window.location.href = '/index.html'
     }    
 }
